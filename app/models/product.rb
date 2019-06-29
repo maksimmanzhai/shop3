72d38af224629f4ee93904dd42c0a5d3d3366b90
maksimmanzhai <bender23max@gmail.com>
2019-06-29 21:42:06 +0300

    Add validation uniqueness

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { in: 2..20 }
+  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :price, :presence => true
   validates :weight, numericality: true
 end
