dd29dc0b6a95e9bd35338dc82c65d53d59fcbf08
maksimmanzhai <bender23max@gmail.com>
2019-07-01 21:44:54 +0300

    Adding to validation inclusion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
-  validates :price, :presence => true
-  validates :weight, numericality: true
+  validates :name, presence: true # , length: { in: 2..20 }, uniqueness: true
+  validates :in_stock, inclusion: { in: [true, false] }
+  # validates :price, :presence => true
+  # validates :weight, numericality: true
 end
